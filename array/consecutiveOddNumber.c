bfffa9af0ddd90d8334009361d308fbe997208c6
shukumar-542 <shukumar542@gmail.com>
2025-01-29 22:54:29 +0600

    Solve consecutive odd number add

---
+++ array/consecutiveOddNumber.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 1 ; i <= n ;  i++){
+        int x = 0;
+        int y = 0;
+        scanf("%d", &x);
+        scanf("%d" , &y);
+
+        if(x > y){
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        int sum  = 0;
+        for(int j = x+1 ; j < y ; j++ ){
+            if(j % 2 !=0){
+                sum += j;
+            }
+        }
+        printf("%d \n", sum);
+    }
+
+    return 0;
+}
